66e0377f7a3bde39aa4cf127ac5fddfb95b4b7be
Prashant <prashantsoni1242@gmail.com>
2022-11-21 21:18:10 +0530

    Stack

---
+++ stack.js
@@ -0,0 +1,49 @@
+//stack implementation - LIFO data structure.
+
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  show() {
+    if (!this.stack.length) {
+      console.log("stack is empty!");
+    } else {
+      console.log(this.stack);
+    }
+  }
+
+  push(val) {
+    this.stack.push(val);
+  }
+
+  pop() {
+    if (!this.stack[this.stack.length - 1]) {
+      console.log("stack is empty!");
+    } else {
+      let poppedItem = this.stack.pop();
+      return poppedItem;
+    }
+  }
+
+  peek() {
+    if (!this.stack.length) {
+      console.log("stack is empty!");
+    } else {
+      return this.stack[this.stack.length - 1];
+    }
+  }
+}
+
+//working
+let stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+stack.show();
+
+stack.peek();
+
+
